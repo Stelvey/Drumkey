999281635ba0b88ac30cd344d98e3c80621aa2c8
Dan Hono <stelveflame@gmail.com>
2024-05-04 17:12:06 +0400

    Implement an easter egg and extra improvements

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 // Attempt at preloading audio so that there's no "first delay"
-clap = new Audio('media/clap.wav');
-hihat = new Audio('media/hihat.wav');
-kick = new Audio('media/kick.wav');
-percussion = new Audio('media/percussion.wav');
-ride = new Audio('media/ride.wav');
-rimshot = new Audio('media/rimshot.wav');
-shaker = new Audio('media/shaker.wav');
-snap = new Audio('media/snap.wav');
-snare = new Audio('media/snare.wav');
+clap = new Audio('media/drumkey/clap.wav');
+hihat = new Audio('media/drumkey/hihat.wav');
+kick = new Audio('media/drumkey/kick.wav');
+percussion = new Audio('media/drumkey/percussion.wav');
+ride = new Audio('media/drumkey/ride.wav');
+rimshot = new Audio('media/drumkey/rimshot.wav');
+shaker = new Audio('media/drumkey/shaker.wav');
+snap = new Audio('media/drumkey/snap.wav');
+snare = new Audio('media/drumkey/snare.wav');
+can = new Audio('media/drunkey/can.wav');
 
 // The very thing that makes keys go brr
-function triggerKey(key, state) {
+let triggerKey = (key, state) => {
     if (state) {
-        new Audio(`media/${key.lastElementChild.textContent.toLowerCase()}.wav`).play();
+        new Audio(`media/${title.textContent.toLowerCase()}/${key.lastElementChild.textContent.toLowerCase()}.wav`).play();
         key.classList.add('played');
     } else {
         key.classList.remove('played');
@@ -34,15 +35,12 @@ for (key of keys) {
     // Mouse controls
     key.addEventListener('mousedown', e => {
         triggerKey(e.currentTarget, 1);
-        e.preventDefault();
     });
     key.addEventListener('mouseup', e => {
         triggerKey(e.currentTarget, 0);
-        e.preventDefault();
     })
     key.addEventListener('mouseleave', e => {
         triggerKey(e.currentTarget, 0);
-        e.preventDefault();
     })
 
     // Touch controls
@@ -54,8 +52,32 @@ for (key of keys) {
         triggerKey(e.currentTarget, 0);
         e.preventDefault();
     })
-    key.addEventListener('touchmove', e => {
+    // IF YOU MOVE YOUR FINGER OFF IT IS STILL KINDA ACTIVE
+    key.addEventListener('touchcancel', e => {
         triggerKey(e.currentTarget, 0);
         e.preventDefault();
     })
 }
+
+// Dumb easter egg
+function checkTitle(title) {
+    switch (title) {
+        case 'Drumkey':
+            const soundpack = ['clap', 'hihat', 'kick', 'percussion', 'ride', 'rimshot', 'shaker', 'snap', 'snare'];
+            for (let i = 0; i < keys.length; i++) {
+                keys[i].lastElementChild.textContent = soundpack[i];
+            }
+            return true;
+        case 'Drunkey':
+            for (let i = 0; i < keys.length; i++) {
+                keys[i].lastElementChild.textContent = 'can';
+            }
+            return true;
+    }
+}
+title = document.querySelector('header .primary');
+title.addEventListener('input', e => {
+    if (checkTitle(e.target.textContent)) {
+        e.target.blur();
+    }
+})
